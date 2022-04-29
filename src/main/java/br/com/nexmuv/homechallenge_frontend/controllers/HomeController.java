613816188d766c7fe95f2d52fd8ba3114c9ff0c4
Douglas Oliveira <douglas.dno@gmail.com>
2022-04-29 11:33:19 -0300

    shutdown

--- src/main/java/br/com/nexmuv/homechallenge_frontend/controllers/HomeController.java
+++ src/main/java/br/com/nexmuv/homechallenge_frontend/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package br.com.nexmuv.homechallenge_frontend.controllers;
 
-import br.com.nexmuv.homechallenge_frontend.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,21 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "")
 public class HomeController {
 
+    @Autowired
+    private ApplicationContext appContext;
+
     @GetMapping("")
     public ModelAndView index() {
         ModelAndView mv = new ModelAndView("index");
         return mv;
     }
+
+    @GetMapping("/shutdown")
+    public ModelAndView shutdown() {
+        SpringApplication.exit(appContext, () -> 1);
+        System.out.println("Spring Boot Aplication is closing......");
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+
 }
